4f12707b1640faedcee8e5b86b533a2aee374fa2
Soham <sohamk2297@gmail.com>
2019-11-02 21:15:22 +0530

    first commit

---
+++ src/main/java/com/juneworks/controllers/MainController.java
@@ -0,0 +1,9 @@
+package com.juneworks.controllers;
+
+import org.springframework.stereotype.Controller;
+
+public class MainController {
+
+	
+	
+}
---
+++ src/main/java/com/juneworks/mainapp/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.juneworks.mainapp;
+
+public class HelloWorld {
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public HelloWorld(String message) {
+		super();
+		this.message = message;
+	}
+	
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
